b398f07688a2d4eefbca60fb60fd4fd7bc48c36c
e-kondra <e-kondra@mail.ru>
2022-01-28 14:23:06 +0200

    Lesson5 Logging

--- server.py
+++ server.py
@@ -1,19 +1,25 @@
 import json
 import sys
+import logging
 from socket import socket, AF_INET, SOCK_STREAM
 
 from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS,PRESENCE, TIME, USER, ERROR, DEFAULT_PORT
 from common.utils import get_message, send_message
+from logs.configs import server_log_config
 
+LOG = logging.getLogger('server')
 
 def check_message(msg):
     if ACTION in msg and msg[ACTION] == PRESENCE and TIME in msg and USER in msg and msg[USER][ACCOUNT_NAME] == 'Guest':
+        LOG.info('Проверка сообщения в check_message успешна, ответ: 200')
         return {RESPONSE: 200}
+    LOG.warning('Проверка сообщения в check_message не успешна, ответ: Bad request ')
     return {RESPONSE: 400, ERROR: 'Bad request'}
 
 
 def main():
-    # server.py -p 8888 -a 192.168.1.33
+    # server.py -p 8880 -a 192.168.56.1
+
     try:
         if '-p' in sys.argv:
             listen_port = int(sys.argv[sys.argv.index('-p') + 1])
@@ -22,10 +28,10 @@ def main():
         if listen_port < 1024 or listen_port > 65535:
             raise ValueError
     except IndexError:
-        print('После параметра -\'p\' необходимо указать номер порта.')
+        LOG.critical('Проверка порта: После параметра -\'p\' необходимо указать номер порта.')
         sys.exit(1)
     except ValueError:
-        print(
+        LOG.critical(
             'В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
         sys.exit(1)
 
@@ -36,11 +42,11 @@ def main():
             listen_address = ''
 
     except IndexError:
-        print(
+        LOG.critical(
             'После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
         sys.exit(1)
     # create servers socket
-    print(listen_address, listen_port)
+
     server_socket = socket(AF_INET, SOCK_STREAM)
     server_socket.bind((listen_address, listen_port, ))
 
@@ -50,12 +56,14 @@ def main():
         client_socket, client_addr = server_socket.accept()
         try:
             clients_message = get_message(client_socket)
-            print(clients_message)
+            LOG.info(f'Получено сообщение {clients_message}')
             response = check_message(clients_message)
+            LOG.info(f'Сообщение проверено, сформирован ответ: {response}')
             send_message(client_socket, response)
+            LOG.info(f'Ответ отправлен, соединениe с клиентом {client_addr} закрывается')
             client_socket.close()
         except (ValueError, json.JSONDecodeError):
-            print('Принято некорретное сообщение от клиента.')
+            LOG.error('Принято некорретное сообщение от клиента.')
             client_socket.close()
 
 
